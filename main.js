ae11764b9af96e140002975b4da2473f1c9b1d50
reirodrigues <reinaldorodrigues13@yahoo.com>
2022-05-31 17:03:21 -0300

    Atalho teclado com javascript

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
+document.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    const btn = document.querySelector('#click')
+
+    btn.click()
+  }
+})
+
 function show() {
-   showBackToTopButtonOnScroll()
+  showBackToTopButtonOnScroll()
 }
 
- function showBackToTopButtonOnScroll(){
-   if(scrollY>500){
-     backToTopButton.classList.add("show")
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
   } else {
-    backToTopButton.classList.remove("show")
-   }
- }
+    backToTopButton.classList.remove('show')
+  }
+}
